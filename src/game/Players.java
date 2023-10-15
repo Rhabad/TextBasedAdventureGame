8b4465c482ed8cc6de3aef925c2b81509e87380d
Rhabad <nicolaspizarro.450@gmail.com>
2023-10-14 23:52:39 -0300

    mostrar y elegir clase

--- src/game/Players.java
+++ src/game/Players.java
@@ -1,18 +1,70 @@
 package game;
 
+import java.util.Scanner;
+
 public class Players {
     private String name;
     private String race;
-    private String specialitation;
+    private String[] specialitation = {"Guerrero", "Mago", "Picaro", "Sacerdote", "Cazador"};
     private int level;
 
     private final int levelForDefect = 1;
 
-    public Players(String name, String race, String specialitation){
+    public Players(String name, String race){
         this.name = name;
         this.race = race;
-        this.specialitation = specialitation;
         this.level = levelForDefect;
     }
 
+    public void mostrarClases(){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("----------Las Clases-----------");
+        System.out.println("1. Guerrero\n" +
+                "2. Mago\n" +
+                "3. Picaro\n" +
+                "4. Sacerdote\n" +
+                "5. Cazador");
+        System.out.print("Elige: ");
+        int eleccion = entrada.nextInt()-1;
+
+        switch (eleccion){
+            case 1:
+                System.out.println("Guerrero");
+                System.out.println("tipico cuerpo a cuerpo");
+                break;
+            case 2:
+                System.out.println("Mago");
+                System.out.println("lanza magia, no pienses mucho");
+                break;
+            case 3:
+                System.out.println("Picaro");
+                System.out.println("usa dagas... le gusta robar, la muy rata");
+                break;
+            case 4:
+                System.out.println("Sacerdote");
+                System.out.println("un sanador de toda la vida");
+                break;
+            case 5:
+                System.out.println("Cazador");
+                System.out.println("No se... un rastreador y usa un arco, suele caer en trampas");
+                break;
+            default:
+                System.out.println("Por favor escoja un numero acorde a la clase.");
+                break;
+        }
+    }
+
+    public String elegirClase(){
+        return "algo eligira";
+    }
+
+
+    @Override
+    public String toString(){
+        return "Nombre: "+this.name+"\n" +
+                "Raza: "+this.race+"\n" +
+                "Clase: "+this.specialitation+"\n" +
+                "NIvel: "+this.level;
+    }
 }

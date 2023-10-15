5dcabb29998027b0af31856a80e2602b3b5d2fd0
Rhabad <nicolaspizarro.450@gmail.com>
2023-10-14 23:07:15 -0300

    text based game adventure

---
+++ src/Players.java
@@ -0,0 +1,16 @@
+public class Players {
+    private String name;
+    private String race;
+    private String specialitation;
+    private int level;
+
+    private final int levelForDefect = 1;
+
+    public Players(String name, String race, String specialitation){
+        this.name = name;
+        this.race = race;
+        this.specialitation = specialitation;
+        this.level = levelForDefect;
+    }
+
+}

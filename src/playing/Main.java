b02f26281a7ecc0c687a8b0761a755fb34c716f8
Rhabad <nicolaspizarro.450@gmail.com>
2023-10-17 18:54:51 -0300

    realizando el menu

--- src/playing/Main.java
+++ src/playing/Main.java
@@ -3,11 +3,37 @@ package playing;
 import game.Players;
 import game.Util;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         Util util = new Util();
 
-        util.mostrarClases();
+        do {
+            System.out.println("----- MENU -----");
+            System.out.println("1. Crear personaje\n" +
+                    "2. Mostrar clases\n" +
+                    "3. Salir");
+            int opcion = entrada.nextInt();
+
+            switch (opcion){
+                case 1:
+                    
+                    break;
+                case 2:
+                    util.mostrarClases();
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
+
+        }while ();
+
+
+
 
     }
 }

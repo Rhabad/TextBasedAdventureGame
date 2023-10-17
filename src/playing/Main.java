ca5ac744f8cd858a98d58ae856457e77002c45fa
Rhabad <nicolaspizarro.450@gmail.com>
2023-10-17 19:18:04 -0300

    menu, descripcion de clases y creacion personaje

--- src/playing/Main.java
+++ src/playing/Main.java
@@ -1,33 +1,44 @@
 package playing;
 
+import game.Players;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+
         Util util = new Util();
+        Players players = null;
 
         do {
             System.out.println("----- MENU -----");
             System.out.println("1. Crear personaje\n" +
-                    "2. Mostrar clases\n" +
-                    "3. Salir");
+                    "2. Mostrar clases");
             int opcion = entrada.nextInt();
 
             switch (opcion){
                 case 1:
+                    System.out.print("Nombre: ");
+                    String nombre = entrada.next();
+                    System.out.print("Raza: ");
+                    String raza = entrada.next();
 
+                    //se crea el personaje
+                    players = new Players(nombre, raza);
+                    players.elegirClase();
+
+                    System.out.println(players);
                     break;
                 case 2:
                     util.mostrarClases();
                     break;
-                case 3:
-                    break;
                 default:
+                    System.out.println("ERROR, OPCION ERRONEA");
                     break;
             }
 
-        }while ();
+        }while (players == null);
 
 
 
